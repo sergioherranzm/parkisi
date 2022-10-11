dab4757fe8b998186ffea2ed925c414bd6f92b1a
Sergio Herranz <sergioherranzm@gmail.com>
2022-10-11 23:56:16 +0200

    fix: gitignore bis

--- frontend/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/slot/[slot_id]":["static\u002Fchunks\u002Fpages\u002Fslot\u002F[slot_id].js"],sortedPages:["\u002F_app","\u002F_error","\u002Fslot\u002F[slot_id]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
